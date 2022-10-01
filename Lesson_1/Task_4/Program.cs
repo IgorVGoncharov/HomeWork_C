f714ab8bf356532ac533d479e012f79fdc74815f
Igor <igorvgoncharov@mail.ru>
2022-10-01 23:02:51 +0300

    Task_4 added

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+Console.Write("Все четные числа от 1 до N: ");
+while (start <= N)
+{
+    if (start%2 == 0)
+    {
+        if (start == 2)
+        {
+            Console.Write($"{start}");
+        }
+        else
+        {
+            Console.Write($", {start}");
+        }            
+    }
+    start = start + 1;
+}
