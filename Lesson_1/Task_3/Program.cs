288dd0bf9ab27bbfce8c78b0939cf5d868015b0d
Igor <igorvgoncharov@mail.ru>
2022-10-01 22:51:12 +0300

    Task_3 added

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number%2;
+if (result == 0)
+{
+    Console.Write("Введенное число четное");
+}
+else
+{
+    Console.Write("Введенное число не четное");
+}
