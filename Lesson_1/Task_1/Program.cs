a597ab52933a9efdb60200ec6b24542af123adbe
Igor <igorvgoncharov@mail.ru>
2022-10-01 21:24:14 +0300

    Task_1 added

--- Lesson_1/Task_1/Program.cs
+++ Lesson_1/Task_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write($"max = {a}, min = {b} " );
+}
+else if (b > a)
+{
+    Console.Write($"max = {b}, min = {a} " );
+}
+else
+{
+    Console.Write("Число 1 = числу 2");
+}
