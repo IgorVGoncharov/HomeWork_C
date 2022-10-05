ac399bc04ab81a6f0ac68c160a280bb4a0f15aaa
Igor <igorvgoncharov@mail.ru>
2022-10-05 20:18:09 +0300

    folder lesson_2 added

---
+++ Lesson_2/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = Convert.ToString(number).Length;
+if (length != 3)
+{
+    System.Console.WriteLine("Введено не трехзначное число!");
+}
+else
+{
+    System.Console.WriteLine((number / 10) % 10);
+}
