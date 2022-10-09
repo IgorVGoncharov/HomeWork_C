59617e11ed9fe394359e39b0e26a533b290185eb
Igor <igorvgoncharov@mail.ru>
2022-10-09 19:49:40 +0300

    task_3 in lesson_3 added

---
+++ Lesson_3/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int promt(string message) // ввод числа с клавиатуры
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = promt("Введите число: ");
+
+for (int i=1; i <= number; i++)
+{
+    System.Console.Write($"{Math.Pow(i, 3)} ");
+}
+
