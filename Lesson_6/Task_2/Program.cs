794d56535710399d8148e5bcbe6b3a1295cd675b
Igor <igorvgoncharov@mail.ru>
2022-10-23 15:58:32 +0300

    task_3 in lesson_7 added

--- Lesson_6/Task_2/Program.cs
+++ Lesson_6/Task_2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых,
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
@@ -14,23 +14,23 @@ int promt(string message) // ввод числа с клавиатуры
 
 void FindIntersecPoint(double b1, double b2, double k1, double k2) // функция поиска точек пересечения
 {
-    if ((b1-b2) == 0 && (k2-k1) == 0)
+    if ((b1 - b2) == 0 && (k2 - k1) == 0)
     {
         System.Console.WriteLine();
         System.Console.WriteLine("Прямые совпадают");
     }
-    else if ((k2-k1) == 0)
+    else if ((k2 - k1) == 0)
     {
         System.Console.WriteLine();
         System.Console.WriteLine("Прямые паралельны");
     }
     else
     {
-    double x = (b1-b2) / (k2-k1);
-    double y = (k1*x) + b1;
-    System.Console.WriteLine();
-    System.Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
-   }
+        double x = (b1 - b2) / (k2 - k1);
+        double y = (k1 * x) + b1;
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
+    }
 }
 
 double b1 = promt("Введите b1: ");
---
+++ Lesson_7/Task_3/Program.cs
@@ -0,0 +1,64 @@
+﻿//Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+int promt(string message) // ввод числа с клавиатуры
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void PrintMultiArray(int[,] arr) // печать многомерного массива
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine("Двумерный массив случайных чисел:");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] NewArray(int line, int col) // создание нового массива из случаных чисел
+{
+    int[,] newarray = new int[line, col];
+    for (int i = 0; i < newarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newarray.GetLength(1); j++)
+        {
+            newarray[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return newarray;
+}
+
+void SearchForAverage(int[,] arr) // поиск среднего арифметического
+{
+    System.Console.WriteLine("Среднее арифметическое каждого столбца: ");
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        int result = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            result += arr[i, j];
+        }
+        double average = result / (arr.GetLength(0) * 1.0);
+        System.Console.WriteLine($"Столбец {j + 1}: {average:f1} ");
+    }
+}
+
+int line = promt("Введите число строк в массиве: ");
+int column = promt("Введите число столбцов в массиве: ");
+int[,] newarray = NewArray(line, column);
+PrintMultiArray(newarray);
+System.Console.WriteLine();
+SearchForAverage(newarray);
